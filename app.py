fafe796df86fec18f2199e2b70a2ae0e9657da06
vibzz1 <vibhortyagi87@gmail.com>
2026-05-02 11:40:57 +0530

    app.py

--- app.py
+++ app.py
@@ -213,6 +213,7 @@ if st.button("🚀 Run Market Scan", type="primary"):
                                     }
                                 }
                             }
+                        }
     
 
                 renderLightweightCharts([{"chart": chartOptions, "series": series_list}], 'chart_' + res['Ticker'])
